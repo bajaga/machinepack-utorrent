e733d1da0b54d56370b430b023643213fe22efa4
miloban <milosbandobranski@gmail.com>
2017-05-29 22:21:42 +0200

    fixed wrong spelling, added size, downloaded properties, fixed wrong credentials error handling

--- machines/list-torrents.js
+++ machines/list-torrents.js
@@ -49,14 +49,16 @@ module.exports = {
     // console.log(client, typeof client.call, client.call);
     client.call("list", function(err, data) {
       if (err) {
-        return exists.error(err);
+        return exits.error(err);
       }
       var torrents = data.torrents;
       torrents = torrents.map(function(info) {
         return {
           hash: info[0],
           name: info[2],
+          size: info[3],
           percentDone: info[4] / 1000,
+          downloaded: info[5],
           eta: info[10],
           torrentUrl: info[19],
           status: info[21],
